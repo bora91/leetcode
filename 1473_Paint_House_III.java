e20a17692ed763c7305b694ffeb2275f2f2bc0d6
bora91 <baukabai91@gmail.com>
2020-09-13 10:18:02 +0600

    initial commit

--- 1473_Paint_House_III.java
+++ 1473_Paint_House_III.java
@@ -1,6 +1,6 @@
 class Solution {
     public static void main(String[] args) {
-        System.out.println(new Solution().minCost(new int[]{0,0,0,0,0}, new int[][]{{1,10},{10,1},{10,1},{1,10},{5,1}}, 5, 2, 3);
+        System.out.println(new Solution().minCost(new int[]{0,0,0,0,0}, new int[][]{{1,10},{10,1},{10,1},{1,10},{5,1}}, 5, 2, 3));
     }
 
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
