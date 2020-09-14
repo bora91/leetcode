0255eba885a315bfd4b519f74e960dc3ff395a7c
bora91 <baukabai91@gmail.com>
2020-09-14 08:00:56 +0600

    Runtime: 1 ms, faster than 100.00% of Java online submissions for Count Unhappy Friends. Memory Usage: 60.2 MB, less than 80.00% of Java online submissions for Count Unhappy Friends.

--- Contest206.java
+++ Contest206.java
@@ -1,6 +1,6 @@
 public class Contest206 {
     public static void main(String[] args) {
-        System.out.println(new Contest206().numSpecial(new int[][]{{0,0},{0,0},{1,0}}));
+        System.out.println(new Contest206().unhappyFriends(4, new int[][] { { 1, 3, 2 }, { 2, 3, 0 }, { 1, 3, 0 }, {0, 2, 1} }, new int[][]{{1, 3}, {0, 2}}));
     }
 
     public int numSpecial(int[][] mat) {
@@ -35,5 +35,48 @@ public class Contest206 {
         }
 
         return res;
+    }
+
+    public int unhappyFriends(int n, int[][] preferences, int[][] pairs) {
+        int res = 0;
+        int[] map = buildHashMap(pairs, n);
+        for (int[] pair : pairs) {
+            if (isHappy(pair[0], pair[1], preferences, map) != -1) {
+                res += 1;
+            }
+            if (isHappy(pair[1], pair[0], preferences, map) != -1) {
+                res += 1;
+            }
+        }
+        return res;
+    }
+
+    private int isHappy(int person, int curMate, int[][] preferences, int[] map) {
+
+        for (int mate : preferences[person]) {
+            if (mate == curMate)
+                return -1;
+
+            int matesCurMate = map[mate];
+            for (int matesMate : preferences[mate]) {
+                if (matesMate == person) {
+                    return mate;
+                } else if (matesMate == matesCurMate) {
+                    break;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private int[] buildHashMap(int[][] pairs, int n) {
+        int[] map = new int[n];
+        for (int[] pair : pairs) {
+            map[pair[0]] = pair[1];
+            map[pair[1]] = pair[0];
+        }
+
+        return map;
     }
 }
