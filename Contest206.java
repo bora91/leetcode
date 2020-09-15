af69d8bea07447aadc2e03a001beb944e5bda6d0
bora91 <baukabai91@gmail.com>
2020-09-15 08:36:24 +0600

    Runtime: 130 ms, faster than 82.53% of Java online submissions for Min Cost to Connect All Points. Memory Usage: 61 MB, less than 76.79% of Java online submissions for Min Cost to Connect All Points.

--- Contest206.java
+++ Contest206.java
@@ -1,6 +1,7 @@
+import java.util.*;
 public class Contest206 {
     public static void main(String[] args) {
-        System.out.println(new Contest206().unhappyFriends(4, new int[][] { { 1, 3, 2 }, { 2, 3, 0 }, { 1, 3, 0 }, {0, 2, 1} }, new int[][]{{1, 3}, {0, 2}}));
+        System.out.println(new Contest206().minCostConnectPoints(new int[][]{{0,0},{2,2},{3,10},{5,2},{7,0}}));
     }
 
     public int numSpecial(int[][] mat) {
@@ -78,5 +79,50 @@ public class Contest206 {
         }
 
         return map;
+    }
+
+    int[] parent;
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        int[][]distance = new int[n][n];
+        parent = new int[n];
+        
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+        
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->distance[a[0]][a[1]]-distance[b[0]][b[1]]);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                distance[i][j] = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
+                pq.offer(new int[]{i, j});
+            }
+        }
+        
+        int count = n;
+        int res = 0;
+        while(!pq.isEmpty() && count > 1){
+            int[]p = pq.poll();
+            if(union(p[0], p[1])){
+                res += distance[p[0]][p[1]];
+                count--;
+            }
+        }
+        
+        return res;
+    }
+    boolean union(int x, int y){
+        int parentx = find(x);
+        int parenty = find(y);
+        if(parentx == parenty)
+            return false;
+        parent[parentx] = parenty;
+        return true;
+    }
+    int find(int x){
+        if(parent[x] != x)
+            parent[x] = find(parent[x]);
+        
+        return parent[x];
     }
 }
