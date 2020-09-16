544b4191c832e0cee6d23c98fc2d280a21d237b7
bora91 <baukabai91@gmail.com>
2020-09-16 08:55:24 +0600

    Runtime: 127 ms, faster than 11.62% of Java online submissions for Check If String Is Transformable With Substring Sort Operations. Memory Usage: 60.4 MB, less than 11.98% of Java online submissions for Check If String Is Transformable With Substring Sort Operations.

--- Contest206.java
+++ Contest206.java
@@ -1,7 +1,9 @@
 import java.util.*;
+
 public class Contest206 {
     public static void main(String[] args) {
-        System.out.println(new Contest206().minCostConnectPoints(new int[][]{{0,0},{2,2},{3,10},{5,2},{7,0}}));
+        System.out.println(new Contest206()
+                .isTransformable("84532", "34852"));
     }
 
     public int numSpecial(int[][] mat) {
@@ -82,47 +84,73 @@ public class Contest206 {
     }
 
     int[] parent;
+
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
-        int[][]distance = new int[n][n];
+        int[][] distance = new int[n][n];
         parent = new int[n];
-        
-        for(int i = 0; i < n; i++)
+
+        for (int i = 0; i < n; i++)
             parent[i] = i;
-        
-        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->distance[a[0]][a[1]]-distance[b[0]][b[1]]);
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                distance[i][j] = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
-                pq.offer(new int[]{i, j});
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> distance[a[0]][a[1]] - distance[b[0]][b[1]]);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                distance[i][j] = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                pq.offer(new int[] { i, j });
             }
         }
-        
+
         int count = n;
         int res = 0;
-        while(!pq.isEmpty() && count > 1){
-            int[]p = pq.poll();
-            if(union(p[0], p[1])){
+        while (!pq.isEmpty() && count > 1) {
+            int[] p = pq.poll();
+            if (union(p[0], p[1])) {
                 res += distance[p[0]][p[1]];
                 count--;
             }
         }
-        
+
         return res;
     }
-    boolean union(int x, int y){
+
+    boolean union(int x, int y) {
         int parentx = find(x);
         int parenty = find(y);
-        if(parentx == parenty)
+        if (parentx == parenty)
             return false;
         parent[parentx] = parenty;
         return true;
     }
-    int find(int x){
-        if(parent[x] != x)
+
+    int find(int x) {
+        if (parent[x] != x)
             parent[x] = find(parent[x]);
-        
+
         return parent[x];
+    }
+
+    public boolean isTransformable(String s, String t) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] cnt = new int[10];
+        for (int i = 0; i < 10; i++)
+            map.put(i, new ArrayList<>());
+        for (int i = 0; i < s.length(); i++) {
+            map.get(s.charAt(i) - '0').add(i);
+        }
+        for (char ch : t.toCharArray()) {
+            int d = ch - '0';
+            if (cnt[d] >= map.get(d).size())
+                return false;
+            int positionOfd = map.get(d).get(cnt[d]);
+            for (int j = 0; j < d; j++) {
+                if (cnt[j] < map.get(j).size() && map.get(j).get(cnt[j]) < positionOfd)
+                    return false;
+            }
+            cnt[d]++;
+        }
+
+        return true;
     }
 }
