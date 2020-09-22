d647a8715e7e014d5da3e0737cd53b0f0f807dde
bora91 <baukabai91@gmail.com>
2020-09-22 08:11:05 +0600

    dfs

--- Weekly207.java
+++ Weekly207.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class Weekly207 {
+    public static void main(String[] args) {
+        System.out.println(new Weekly207().maxProductPath(new int[][]{{1,-2,1},{1,-2,1},{3,-4,1}}));
+    }
+
     public String reorderSpaces(String text) {
         int words = 0;
         int space = 0;
@@ -71,5 +75,29 @@ public class Weekly207 {
             set.remove(sub);
         }
         return res;
+    }
+
+    long p = -1;
+
+    public int maxProductPath(int[][] grid) {
+        int mod = 1_000_000_007;
+        dfs(grid, 0, 0, grid[0][0]);
+        return (int) (p % mod);
+    }
+
+    void dfs(int[][] grid, int i, int j, long curr) {
+        if (i == grid.length - 1 && j == grid[0].length - 1) {
+            p = Math.max(p, curr);
+            return;
+        }
+        if (grid[i][j] == 0) {
+            p = Math.max(p, 0);
+            return;
+        }
+
+        if (i + 1 < grid.length)
+            dfs(grid, i + 1, j, curr * grid[i + 1][j]);
+        if (j + 1 < grid[0].length)
+            dfs(grid, i, j + 1, curr * grid[i][j + 1]);
     }
 }
